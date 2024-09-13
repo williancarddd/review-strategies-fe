440b3ef6c9d7986e9b570d071c8404d1854c7668
william cardoso barbosa <williancard123@gmail.com>
2024-09-13 14:18:51 -0400

    chore: add analitycs and add meta graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ReactNode } from 'react';
 import RouteGuard from '@/components/RouteGuard/route-guard';
 import { Provider } from '@/providers/provider';
 import { Metadata } from 'next';
+import { Analytics } from "@vercel/analytics/react"
 
 // Configurar as fontes usando o pacote next/font/google
 const vibur = Vibur({
@@ -40,8 +41,18 @@ export default function RootLayout({ children }: RootLayoutProps) {
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/favicon.ico" sizes='32x32' />
+        <meta property="twitter:image" content="Twitter link preview image URL"></meta>
+        <meta property="twitter:card" content="summary_large_image"></meta>
+        <meta property="twitter:title" content="Twitter link preview title"></meta>
+        <meta property="twitter:description" content="Twitter link preview description"></meta>
+        <meta property="og:image" content="Link preview image URL"></meta>
+        <meta property="og:site_name" content="Link preview site name"></meta>
+        <meta property="og:title" content="Link preview title"></meta>
+        <meta property="og:description" content="Link preview description" />
+        <meta property="og:url" content="Canonical link preview URL"></meta>
       </head>
       <body
+
         className={cn(
           'min-h-screen bg-background font-sans antialiased',
           vibur.variable, 
@@ -52,6 +63,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <Provider>
           <RouteGuard>{children}</RouteGuard>
         </Provider>
+        <Analytics />
       </body>
     </html>
   );
