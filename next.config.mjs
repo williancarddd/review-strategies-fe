1d375e69a15e36b394d35162500110db07b607af
William Cardoso <williancard123@Gmail.com>
2024-09-16 20:06:09 -0400

    config: fix deplou fonci

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+  // cors
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, Content-Type, Accept, Origin',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
