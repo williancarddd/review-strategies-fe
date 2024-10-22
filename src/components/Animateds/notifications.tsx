d21110e926698ae416cebcc91bfd74ba18ba5ce2
william cardoso <williancard123@gmail.com>
2024-10-22 17:33:11 -0400

    fix

--- src/components/Animateds/notifications.tsx
+++ src/components/Animateds/notifications.tsx
@@ -1,8 +1,9 @@
-"use client";
+'use client';
 
+import { FC } from 'react';
+import { useTranslations } from 'next-intl';
 import { cn } from "@/lib/utils";
 import { AnimatedList } from "../ui/animated-list";
-import WhatssapIcon from "../Icons/whatssap";
 
 interface Item {
   name: string;
@@ -12,36 +13,14 @@ interface Item {
   time: string;
 }
 
-let notifications = [
-  {
-    name: "Chegou a hora de revisar",
-    description: "O seu conteúdo de revisão é Geografia",
-    time: "15m ago",
-    icon: "💸",
-    color: "#00C9A7",
-  },
-  {
-    name: "Você vai revisar daqui a pouco",
-    description: "O seu conteúdo de revisão é Matemática",
-    time: "10m ago",
-    icon: "👤",
-    color: "#FFB800",
-  },
-];
-
-notifications = Array.from({ length: 10 }, () => notifications).flat();
-
-const Notification = ({ name, description, icon, color, time }: Item) => {
+const Notification: FC<Item> = ({ name, description, icon, color, time }) => {
   return (
     <figure
       className={cn(
         "relative mx-auto min-h-fit w-full max-w-[400px] cursor-pointer overflow-hidden rounded-2xl p-4",
-        // animation styles
         "transition-all duration-200 ease-in-out hover:scale-[103%]",
-        // light styles
         "bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]",
-        // dark styles
-        "transform-gpu dark:bg-transparent dark:backdrop-blur-md dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
+        "dark:bg-transparent dark:backdrop-blur-md dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]"
       )}
     >
       <div className="flex flex-row items-center gap-3">
@@ -54,32 +33,40 @@ const Notification = ({ name, description, icon, color, time }: Item) => {
           <span className="text-lg">{icon}</span>
         </div>
         <div className="flex flex-col overflow-hidden">
-          <figcaption className="flex flex-row items-center whitespace-pre text-lg font-medium dark:text-white ">
+          <figcaption className="flex flex-row items-center whitespace-pre text-lg font-medium dark:text-white">
             <span className="text-sm sm:text-lg">{name}</span>
             <span className="mx-1">·</span>
             <span className="text-xs text-gray-500">{time}</span>
           </figcaption>
-          <p className="text-sm font-normal dark:text-white/60">
-            {description}
-          </p>
+          <p className="text-sm font-normal dark:text-white/60">{description}</p>
         </div>
       </div>
     </figure>
   );
 };
 
-export function AnimatedListDemo({
-  className,
-}: {
-  className?: string;
-}) {
+export const AnimatedListDemo: FC<{ className?: string }> = ({ className }) => {
+  const t = useTranslations('HomePage.notifications');
+
+  const notifications = [
+    {
+      name: t('item1.name'),
+      description: t('item1.description'),
+      time: t('item1.time'),
+      icon: t('item1.icon'),
+      color: t('item1.color'),
+    },
+    {
+      name: t('item2.name'),
+      description: t('item2.description'),
+      time: t('item2.time'),
+      icon: t('item2.icon'),
+      color: t('item2.color'),
+    }
+  ];
+
   return (
-    <div
-      className={cn(
-        "relative flex h-[500px] w-full flex-col p-6 overflow-hidden rounded-lg border bg-background md:shadow-xl",
-        className,
-      )}
-    >
+    <div className={cn("relative flex h-[500px] w-full flex-col p-6 overflow-hidden rounded-lg border bg-background md:shadow-xl", className)}>
       <AnimatedList>
         {notifications.map((item, idx) => (
           <Notification {...item} key={idx} />
@@ -87,4 +74,4 @@ export function AnimatedListDemo({
       </AnimatedList>
     </div>
   );
-}
+};
